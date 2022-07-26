f10f9565e3456cf1cf593fc6c6109600d68258a9
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-26 16:31:01 +0200

    added script for start button and removal of start screen on start pressed

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
+let startButton = document.getElementById('start-button');
+
+startButton.addEventListener('click', ()=>{
+    document.getElementById('start').style.visibility ='hidden';
+    document.getElementById('ground').style.animation = 'autoscroll 3s linear infinite';
+    document.getElementById('treesAndBushes').style.animation = 'autoscroll 6s linear infinite';
+    document.getElementById('distantTrees').style.animation = 'autoscroll 8s linear infinite';
+    document.getElementById('bushes').style.animation = 'autoscroll 10s linear infinite';
+    document.getElementById('hills').style.animation = 'autoscroll 12s linear infinite';
+    document.getElementById('obstacle').style.animation = 'crashCourse 3.5s infinite linear';
+})
+
+
+
 let ghosty = document.getElementById('ghosty');
 let press = false;
 
