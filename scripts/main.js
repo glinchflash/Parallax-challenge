ee9b95c92d32a2d0a038991f7920c286f63c8d05
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-26 15:05:23 +0200

    fixed bugged jumping

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,25 @@
 let ghosty = document.getElementById('ghosty');
+let press = false;
 
 document.addEventListener('keypress', e =>{
-    if (e.key ===  " "){
-        ghosty.classList.add('ghostyJump');
-        console.log('spaced');
+    console.log(press);
+    if (press ){
+        return;
+    } else{
+        if (e.key ===  " "){
+            ghosty.classList.add('ghostyJump');
+        }
+        press = true;
+        console.log(press);
     }
     setTimeout(function (){
         ghosty.classList.remove("ghostyJump");
         ghosty.removeAttribute("style");
     },1400)
+
+    setTimeout(function () {
+        press = false;
+        console.log(press);
+    },1450);
+    console.log(press);
 });
-
-
